34c36af9e4d80d1831f47697d11a74cfe3c583dd
nadiia_aksonova <nadiakswork@gmail.com>
2021-12-21 17:02:34 +0200

    3 tasks on the topic "Arrays"

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,63 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            //  Задание 1 - Вывести массив в обратном порядке
+            int[] myArray = { 3, 100, 12, 9, 0 };
+
+            for (int i = myArray.Length; i > 0; i--)
+            {
+                Console.WriteLine(myArray[i - 1]);
+            }
+
+            //Задание 2 - Заполнить массив из клавиатуры
+
+            Console.WriteLine("Введите количество элементов массива: ");
+
+            int elemets = int.Parse(Console.ReadLine());
+
+            int[] myArray1 = new int[elemets];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write("Введите число в массив: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Массив:");
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+
+            // Задание 3 - Сумма четных чисел
+
+            int[] myArray2 = { 1, 4, 10, 5, 12, 3 };
+
+            int evenNum = 0;
+
+            int oddNum = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                {
+                    evenNum += myArray[i];
+                }
+                else
+                {
+                    oddNum += myArray[i];
+                }
+            }
+
+            Console.WriteLine("Сумма четных чисел = " + evenNum);
+            Console.WriteLine("Сумма нечетных чисел = " + oddNum);
+
+
         }
     }
 }
