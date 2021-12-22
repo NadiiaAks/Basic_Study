65d5206b71d5c71d2b1f57db3b3f750e940c8496
nadiia_aksonova <nadiakswork@gmail.com>
2021-12-22 14:36:22 +0200

    Exercise1_Displaying information about pressed keys

--- enum/Program.cs
+++ enum/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace enum
+namespace enum_homework
 {
     class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+        {
+            //Displaying information about pressed keys
+
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key; //return information about putting key
+
+                int keyCode = (int)key;
+
+                Console.WriteLine($"\tEnum {key}\tKey Code {keyCode}"); // displaying information about pressed key and number 
+
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("You push Enter");
+                }
+                  
+            }
     }
 }
 }
