74ad439a8c119fa103bc158774d989f5f9776324
nadiia_aksonova <nadiakswork@gmail.com>
2021-12-23 12:13:12 +0200

    Add homework_OOP_Basic

---
+++ OOP_basic/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_basic
+{
+    class Car
+    {
+        private int speed = 0;
+
+       // method for displaying speed on consol
+       public void PrintSpeed()
+       {
+            if (speed == 0)
+            {
+                Console.WriteLine("The car doesn't go");
+            }
+            if (speed > 0)
+            {
+                Console.WriteLine($"Driving with speed {speed}");
+            }
+            if (speed < 0)
+            {
+                Console.WriteLine($"Go back with speed {-speed}");
+            }
+
+            
+       }
+
+        // method for start driving
+        public void Driving()
+        {
+            speed = 40;
+        }
+
+        // method for stop car
+        public void Stop()
+        {
+            speed = 0;
+        }
+
+        // method for driving back
+        public void GoBack()
+        {
+            speed = -10;
+        }
+    }
+}
---
+++ OOP_basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car's behavior:");
+            var car = new Car();
+            car.Driving();
+            car.PrintSpeed();
+            car.Stop();
+            car.PrintSpeed();
+            car.GoBack();
+            car.PrintSpeed();
+
+        }
+    }
+}
